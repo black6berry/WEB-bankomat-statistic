0dda27d758199aa025f1b9d72fb81df4da2597b1
MikeVazowskii <mikevazowskii@yandex.ru>
2023-01-23 12:59:58 +0300

    first commit

---
+++ WEB-bankomat-statistic/Model/SignIn.cs
@@ -0,0 +1,8 @@
+﻿namespace WEB_bankomat_statistic.Model
+{
+    public class SignIn
+    {
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WEB-bankomat-statistic/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB_bankomat_statistic.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+        }
+        public string Message { get; set; } = "";
+        
+        public void OnGet()
+        {
+            
+        }
+        public void OnPost(string Phone, string Password)
+        {
+            Message = $"Ваш телефон {Phone}\nВаш пароль {Password}";
+        }
+        
+    }
+}
